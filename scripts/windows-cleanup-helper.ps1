4f4674da46a4bf6defe84eca567956737b711716
Charlie Quick <melvinquick@proton.me>
2024-05-07 11:35:03 -0400

    Adding screenshot folder cleanup call

--- scripts/windows-cleanup-helper.ps1
+++ scripts/windows-cleanup-helper.ps1
@@ -2,8 +2,11 @@ Write-Host("`n--------------------------") -ForegroundColor Green
 Write-Host("| WINDOWS CLEANUP HELPER |") -ForegroundColor Green
 Write-Host("--------------------------") -ForegroundColor Green
 
+# $HOME\Pictures\Screenshot folder cleanup
+Invoke-WebRequest sfc.melvinquick.com | Invoke-Expression
+
 # C:\WIndows\SoftwareDistribution folder reset
 Invoke-WebRequest sdr.melvinquick.com | Invoke-Expression
 
-# C:\WIndows\Temp folder cleanout
+# C:\WIndows\Temp folder cleanup
 Invoke-WebRequest tfc.melvinquick.com | Invoke-Expression
