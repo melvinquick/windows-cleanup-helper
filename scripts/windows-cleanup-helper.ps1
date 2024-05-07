4b41287129ed91bfadc0164f79711bf82c76f795
Charlie Quick <melvinquick@proton.me>
2024-05-07 16:24:23 -0400

    Adding in new script call

--- scripts/windows-cleanup-helper.ps1
+++ scripts/windows-cleanup-helper.ps1
@@ -5,8 +5,11 @@ Write-Host("--------------------------") -ForegroundColor Green
 # $HOME\Pictures\Screenshot folder cleanup
 Invoke-WebRequest sfc.melvinquick.com | Invoke-Expression
 
-# C:\WIndows\SoftwareDistribution folder reset
+# C:\Windows\SoftwareDistribution folder reset
 Invoke-WebRequest sdr.melvinquick.com | Invoke-Expression
 
-# C:\WIndows\Temp folder cleanup
+# C:\Windows\Temp folder cleanup
 Invoke-WebRequest tfc.melvinquick.com | Invoke-Expression
+
+# C Drive Cleanup via CleanMgr.exe
+Invoke-WebRequest cdc.melvinquick.com | Invoke-Expression
