7e7440f48cb325a152c4b4ee31b78f89fea64953
Charlie Quick <melvinquick@proton.me>
2024-05-15 14:35:33 -0400

    Added in rounding math at the end to avoid more than 2 decimal places as it's unnecessary

--- scripts/c-drive-cleanup.ps1
+++ scripts/c-drive-cleanup.ps1
@@ -15,32 +15,32 @@ cleanmgr.exe /$path /VERYLOWDISK /AutoClean
 Write-Host("Calculating space saved...")
 if ($path_size_b_original -lt 1024) {
     $path_size_b_new = Get-Volume -DriveLetter $path | Select-Object @{n = "SizeRemaining"; e = { [math]::Round(($_.SizeRemaining / 1), 2) } } | Select-Object -ExpandProperty SizeRemaining
-    $path_size_b_difference = $path_size_b_new - $path_size_b_original
+    $path_size_b_difference = [math]::Round(($path_size_b_new - $path_size_b_original), 2)
     Write-Host("You saved $path_size_b_difference B of space!")
 }
 elseif ($path_size_kb_original -lt 1024) {
     $path_size_kb_new = Get-Volume -DriveLetter $path | Select-Object @{n = "SizeRemaining"; e = { [math]::Round(($_.SizeRemaining / 1024), 2) } } | Select-Object -ExpandProperty SizeRemaining
-    $path_size_kb_difference = $path_size_kb_new - $path_size_kb_original
+    $path_size_kb_difference = [math]::Round(($path_size_kb_new - $path_size_kb_original), 2)
     Write-Host("You saved $path_size_kb_difference KB of space!")
 }
 elseif ($path_size_mb_original -lt 1024) { 
     $path_size_mb_new = Get-Volume -DriveLetter $path | Select-Object @{n = "SizeRemaining"; e = { [math]::Round(($_.SizeRemaining / 1048576), 2) } } | Select-Object -ExpandProperty SizeRemaining
-    $path_size_mb_difference = $path_size_mb_new - $path_size_mb_original
+    $path_size_mb_difference = [math]::Round(($path_size_mb_new - $path_size_mb_original), 2)
     Write-Host("You saved $path_size_mb_difference MB of space!")
 }
 elseif ($path_size_gb_original -lt 1024) { 
     $path_size_gb_new = Get-Volume -DriveLetter $path | Select-Object @{n = "SizeRemaining"; e = { [math]::Round(($_.SizeRemaining / 1073741824), 2) } } | Select-Object -ExpandProperty SizeRemaining
-    $path_size_gb_difference = $path_size_gb_new - $path_size_gb_original
+    $path_size_gb_difference = [math]::Round(($path_size_gb_new - $path_size_gb_original), 2)
     Write-Host("You saved $path_size_gb_difference GB of space!")
 }
 elseif ($path_size_tb_original -lt 1024) { 
-    $path_size_gb_new = Get-Volume -DriveLetter $path | Select-Object @{n = "SizeRemaining"; e = { [math]::Round(($_.SizeRemaining / 1099511627776), 2) } } | Select-Object -ExpandProperty SizeRemaining
-    $path_size_tb_difference = $path_size_tb_new - $path_size_tb_original
+    $path_size_tb_new = Get-Volume -DriveLetter $path | Select-Object @{n = "SizeRemaining"; e = { [math]::Round(($_.SizeRemaining / 1099511627776), 2) } } | Select-Object -ExpandProperty SizeRemaining
+    $path_size_tb_difference = [math]::Round(($path_size_tb_new - $path_size_tb_original), 2)
     Write-Host("You saved $path_size_tb_difference TB of space!")
 }
 elseif ($path_size_pb_original -lt 1024) { 
     $path_size_pb_new = Get-Volume -DriveLetter $path | Select-Object @{n = "SizeRemaining"; e = { [math]::Round(($_.SizeRemaining / 1125899906842624), 2) } } | Select-Object -ExpandProperty SizeRemaining
-    $path_size_pb_difference = $path_size_pb_new - $path_size_pb_original
+    $path_size_pb_difference = [math]::Round(($path_size_pb_new - $path_size_pb_original), 2)
     Write-Host("You saved $path_size_pb_difference PB of space!")
 }
 else {
--- scripts/downloads-folder-cleanup.ps1
+++ scripts/downloads-folder-cleanup.ps1
@@ -15,32 +15,32 @@ Get-ChildItem -Path $path | Remove-Item -Recurse -Force -WarningAction SilentlyC
 Write-Host("Calculating space saved...")
 if ($path_size_b_original -lt 1024) {
     $path_size_b_new = Get-ChildItem -Path $path -Recurse | Measure-Object -Property Length -Sum | Select-Object @{n = "Size"; e = { [math]::Round(($_.Sum / 1), 2) } } | Select-Object -ExpandProperty Size
-    $path_size_b_difference = $path_size_b_original - $path_size_b_new
+    $path_size_b_difference = [math]::Round(($path_size_b_original - $path_size_b_new), 2)
     Write-Host("You saved $path_size_b_difference B of space!")
 }
 elseif ($path_size_kb_original -lt 1024) {
     $path_size_kb_new = Get-ChildItem -Path $path -Recurse | Measure-Object -Property Length -Sum | Select-Object @{n = "Size"; e = { [math]::Round(($_.Sum / 1024), 2) } } | Select-Object -ExpandProperty Size
-    $path_size_kb_difference = $path_size_kb_original - $path_size_kb_new
+    $path_size_kb_difference = [math]::Round(($path_size_kb_original - $path_size_kb_new), 2)
     Write-Host("You saved $path_size_kb_difference KB of space!")
 }
 elseif ($path_size_mb_original -lt 1024) { 
     $path_size_mb_new = Get-ChildItem -Path $path -Recurse | Measure-Object -Property Length -Sum | Select-Object @{n = "Size"; e = { [math]::Round(($_.Sum / 1048576), 2) } } | Select-Object -ExpandProperty Size
-    $path_size_mb_difference = $path_size_mb_original - $path_size_mb_new
+    $path_size_mb_difference = [math]::Round(($path_size_mb_original - $path_size_mb_new), 2)
     Write-Host("You saved $path_size_mb_difference MB of space!")
 }
 elseif ($path_size_gb_original -lt 1024) { 
     $path_size_gb_new = Get-ChildItem -Path $path -Recurse | Measure-Object -Property Length -Sum | Select-Object @{n = "Size"; e = { [math]::Round(($_.Sum / 1073741824), 2) } } | Select-Object -ExpandProperty Size
-    $path_size_gb_difference = $path_size_gb_original - $path_size_gb_new
+    $path_size_gb_difference = [math]::Round(($path_size_gb_original - $path_size_gb_new), 2)
     Write-Host("You saved $path_size_gb_difference GB of space!")
 }
 elseif ($path_size_tb_original -lt 1024) { 
     $path_size_gb_new = Get-ChildItem -Path $path -Recurse | Measure-Object -Property Length -Sum | Select-Object @{n = "Size"; e = { [math]::Round(($_.Sum / 1099511627776), 2) } } | Select-Object -ExpandProperty Size
-    $path_size_tb_difference = $path_size_tb_original - $path_size_tb_new
+    $path_size_tb_difference = [math]::Round(($path_size_tb_original - $path_size_tb_new), 2)
     Write-Host("You saved $path_size_tb_difference TB of space!")
 }
 elseif ($path_size_pb_original -lt 1024) { 
     $path_size_pb_new = Get-ChildItem -Path $path -Recurse | Measure-Object -Property Length -Sum | Select-Object @{n = "Size"; e = { [math]::Round(($_.Sum / 1125899906842624), 2) } } | Select-Object -ExpandProperty Size
-    $path_size_pb_difference = $path_size_pb_original - $path_size_pb_new
+    $path_size_pb_difference = [math]::Round(($path_size_pb_original - $path_size_pb_new), 2)
     Write-Host("You saved $path_size_pb_difference PB of space!")
 }
 else {
